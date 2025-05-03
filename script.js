604d0ab44ca0818645f130ae0e09c38d6161342e
ithoswow <59713809+ithoswow@users.noreply.github.com>
2025-05-03 12:55:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("paypal-button").onclick = () => {
-  fetch("https://TU_BACKEND_URL/pay/paypal", { method: "POST" })
+  fetch("https://helpful-purpose-production-7db9.up.railway.app/pay/paypal", { method: "POST" })
     .then(res => res.json())
     .then(data => {
       document.getElementById("status").innerText = "Licencia generada: " + data.licencia;
@@ -8,7 +8,7 @@ document.getElementById("paypal-button").onclick = () => {
 };
 
 document.getElementById("pse-button").onclick = () => {
-  fetch("https://TU_BACKEND_URL/pay/pse", { method: "POST" })
+  fetch("https://helpful-purpose-production-7db9.up.railway.app/pay/pse", { method: "POST" })
     .then(res => res.json())
     .then(data => {
       document.getElementById("status").innerText = "Licencia generada: " + data.licencia;
